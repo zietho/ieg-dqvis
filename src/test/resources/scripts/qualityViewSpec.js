d032e71025dd7e99f945eb6955cd87fc092f049f
Thomas Ziegelbecker <e0925321@student.tuwien.ac.at>
2015-03-15 17:49:13 +0100

    added require.js support to karma

---
+++ src/test/resources/scripts/qualityViewSpec.js
@@ -0,0 +1,50 @@
+define(['d3','app/qualityview'], function(d3,qv) {
+    describe("createQualityView", function(){
+        it("to be false", function(){
+
+            var qualityView = qv
+                .x(function (d) {
+                    return new Date(+d.date);
+                })
+                .y(function (d) {
+                    return +d.quality;
+                })
+                .height(100)
+                .margin({top: 0, right: 80, bottom: 30, left: 50});
+
+            expect(qualityView).not.toBeNull();
+        })
+    });
+});
+
+
+
+
+/*
+var qualityView = qv
+    .x(function (d) {
+        return new Date(+d.date);
+    })
+    .y(function (d) {
+        return +d.quality;
+    })
+    .height(100)
+    .margin({top: 0, right: 80, bottom: 30, left: 50});
+
+ //get data
+ d3.json(serverUrl + "/get-data?granularity=minute", function (error, json) {
+     if (error) return console.warn(error);
+     var data = d3.select("#visualization")
+     .datum(json.columns);
+     data.call(detailView);
+     d3.select("#visualization")
+     .datum(json.columns[0].values)
+     .call(qualityView);
+     d3.select("#visualization")
+     .datum(json.columns[0].values)
+     .call(timeSliderView);
+ });
+
+
+
+*/
